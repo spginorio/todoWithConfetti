a48a1b7148f89b428701b0d1d5f3cb18a15803ce
spginorio <spginorio@gmail.com>
2024-10-24 13:15:47 +0900

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   android/gradle/wrapper/gradle-wrapper.properties
    	modified:   android/settings.gradle
    	modified:   ios/Flutter/Debug.xcconfig
    	modified:   ios/Flutter/Release.xcconfig
    	new file:   ios/Podfile
    	new file:   ios/Podfile.lock
    	modified:   ios/Runner.xcodeproj/project.pbxproj
    	modified:   ios/Runner.xcworkspace/contents.xcworkspacedata
    	modified:   ios/Runner/AppDelegate.swift
    	modified:   macos/Flutter/Flutter-Debug.xcconfig
    	modified:   macos/Flutter/Flutter-Release.xcconfig
    	new file:   macos/Podfile
    	new file:   macos/Podfile.lock
    	modified:   macos/Runner.xcodeproj/project.pbxproj
    	modified:   macos/Runner.xcworkspace/contents.xcworkspacedata

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+// import awesome_notifications
+// import shared_preferences_ios
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,3 +13,22 @@ import UIKit
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+// override func application(
+//     _ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//   ) -> Bool {
+//       GeneratedPluginRegistrant.register(with: self)
+
+//       // This function registers the desired plugins to be used within a notification background action
+//       SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in          
+//           SwiftAwesomeNotificationsPlugin.register(
+//             with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)          
+//           FLTSharedPreferencesPlugin.register(
+//             with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
+//       }
+
+//       return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//   }
+
+
